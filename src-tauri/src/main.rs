ebb32a5d9a22e0cb03192967fc59b2bc3026a95e
Seitaro <taha.seita@gmail.com>
2023-11-26 21:55:37 +0900

    add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, AboutMetadata};
+
 pub mod screenshot;
 
 #[tauri::command]
@@ -11,7 +13,48 @@ async fn take_screenshot() -> String {
 }
 
 fn main() {
+    let about = CustomMenuItem::new("about".to_string(), "About");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let mainmenu = Submenu::new(
+        "Compresshot",
+        Menu::new()
+            .add_native_item(MenuItem::About(
+                "compresshot".to_string(),
+                AboutMetadata::default(),
+            ))
+            .add_native_item(MenuItem::Hide)
+            .add_native_item(MenuItem::HideOthers)
+            .add_native_item(MenuItem::ShowAll)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Quit)
+            .add_native_item(MenuItem::Separator)
+            .add_item(about)
+            .add_native_item(MenuItem::Separator)
+            .add_item(quit),
+    );
+    let screenmenu = Submenu::new(
+        "Window",
+        Menu::new()
+            .add_native_item(MenuItem::EnterFullScreen)
+            .add_native_item(MenuItem::Minimize)
+            .add_native_item(MenuItem::Zoom)
+            .add_native_item(MenuItem::CloseWindow),
+    );
+    let menu = Menu::new()
+        .add_submenu(mainmenu)
+        .add_submenu(screenmenu);
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "about" => {
+                let window = event.window();
+                window.emit("about", "Compresshot".to_string()).unwrap();
+            }
+            "quit" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![take_screenshot])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { convertFileSrc, invoke } from "@tauri-apps/api/tauri";
 import { save } from "@tauri-apps/api/dialog";
 import "./App.css";
 import { copyFile } from "@tauri-apps/api/fs";
+import { listen } from "@tauri-apps/api/event";
 
 function App() {
   const [previewImgPath, setPreviewImgPath] = useState("/tauri.svg");
   const [baseImgPath, setBaseImgPath] = useState("");
+
+  useEffect(() => {
+    let unlisten: any;
+    async function f() {
+      unlisten = await listen('about', event => {
+        console.log(`about ${event.payload} ${new Date()}`)
+      });
+    }
+    f();
+
+    return () => {
+      if (unlisten) {
+        unlisten();
+      }
+    }
+  }, [])
 
   async function screenShot() {
     const localImgPath: string = await invoke("take_screenshot");
